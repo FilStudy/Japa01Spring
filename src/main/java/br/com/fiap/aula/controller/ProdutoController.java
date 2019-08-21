1b81bc89cd2ca904bd8c226923d7db5aa9fe1dd0
Logon Aluno <logonaluno@local.com>
2019-08-21 11:06:57 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/aula/controller/ProdutoController.java
@@ -0,0 +1,22 @@
+package br.com.fiap.aula.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("produto")
+public class ProdutoController {
+    @GetMapping("cadastrar")
+    public String abrirFormulario(){
+        return "produto/form";
+    }
+    @PostMapping("cadastrar")
+    public String processarForm(Produto produto, Model model){
+        model.addAttribute("prod",produto);
+        return "produto/sucesso";
+    }
+}
+
